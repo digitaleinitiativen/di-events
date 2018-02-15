f4eb67bb855484764e82a368b2e820f0f257c58a
Christoph Klocker <christoph@vedanova.com>
2018-02-15 22:35:54 +0100

    added config.ru

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require 'sinatra'
 require 'httparty'
 require 'json'
 
+
+require 'sinatra/base'
+
+class App < Sinatra::Base
+
 get '/' do
   erb :index
 end
@@ -11,4 +16,6 @@ get '/events' do
   response = HTTParty.get('https://api.meetup.com/Digitale-Initativen/events?photo-host=public&page=20&sig_id=8826366&sig=8ab18e6ab2055b866e5359fa12c12e50a969f748')
   content_type :json
   { events: response.parsed_response }.to_json
+end
+
 end
